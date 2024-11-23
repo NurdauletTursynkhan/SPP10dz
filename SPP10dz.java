22bd909c7c9b5c00ae3c4aebe76f7318d2c1bdfd
NurdauletTursynkhan <nurdatursn@gmail.com>
2024-11-23 10:10:33 +0500

    Update SPP10dz.java

--- SPP10dz.java
+++ SPP10dz.java
@@ -1,12 +1,9 @@
-// Фасадный класс для управления мультимедиа-центром
 class HomeTheaterFacade {
-    // Подсистемы мультимедиа-центра
     private TV tv;
     private AudioSystem audioSystem;
     private DVDPlayer dvdPlayer;
     private GameConsole gameConsole;
 
-    // Конструктор фасада
     public HomeTheaterFacade() {
         this.tv = new TV();
         this.audioSystem = new AudioSystem();
@@ -14,7 +11,6 @@ class HomeTheaterFacade {
         this.gameConsole = new GameConsole();
     }
 
-    // Метод для начала просмотра фильма
     public void watchMovie() {
         tv.on();
         audioSystem.on();
@@ -22,7 +18,6 @@ class HomeTheaterFacade {
         System.out.println("Приятного просмотра фильма!");
     }
 
-    // Метод для завершения просмотра фильма
     public void endMovie() {
         dvdPlayer.stop();
         audioSystem.off();
@@ -30,7 +25,6 @@ class HomeTheaterFacade {
         System.out.println("Просмотр фильма завершен.");
     }
 
-    // Метод для запуска игры
     public void playGame() {
         tv.on();
         gameConsole.on();
@@ -38,7 +32,6 @@ class HomeTheaterFacade {
         System.out.println("Приятной игры!");
     }
 
-    // Метод для прослушивания музыки
     public void listenToMusic() {
         tv.on();
         audioSystem.on();
@@ -46,41 +39,35 @@ class HomeTheaterFacade {
         System.out.println("Приятного прослушивания музыки!");
     }
 
-    // Метод для регулировки громкости
     public void setVolume(int level) {
         audioSystem.setVolume(level);
         System.out.println("Громкость установлена на " + level);
     }
 
-    // Подсистема TV
     class TV {
         public void on() { System.out.println("Телевизор включен"); }
         public void off() { System.out.println("Телевизор выключен"); }
         public void setChannel(int channel) { System.out.println("Канал установлен на " + channel); }
     }
 
-    // Подсистема AudioSystem
     class AudioSystem {
         public void on() { System.out.println("Аудиосистема включена"); }
         public void off() { System.out.println("Аудиосистема выключена"); }
         public void setVolume(int level) { System.out.println("Громкость аудиосистемы установлена на " + level); }
     }
 
-    // Подсистема DVDPlayer
     class DVDPlayer {
         public void play() { System.out.println("DVD воспроизводится"); }
         public void pause() { System.out.println("DVD на паузе"); }
         public void stop() { System.out.println("DVD остановлен"); }
     }
 
-    // Подсистема GameConsole
     class GameConsole {
         public void on() { System.out.println("Игровая консоль включена"); }
         public void playGame() { System.out.println("Игра запущена"); }
     }
 }
 
-// Клиентский код для демонстрации работы фасада
 public class Main {
     public static void main(String[] args) {
         HomeTheaterFacade homeTheater = new HomeTheaterFacade();
